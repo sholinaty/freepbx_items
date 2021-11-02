ca59d606541370e4a1d3ab8182e6127c1d0e3a77
Stephen Holinaty <sholinaty@vendasta.com>
2021-11-02 10:40:43 -0600

    Create FindMeFollowGet.php

---
+++ php_scripts/FindMeFollowGet.php
@@ -0,0 +1,12 @@
+// run this script as $sudo -u asterisk php /path/to/testGetUser.php
+
+//set up the environment
+<?php
+include '/etc/freepbx.conf';
+$FreePBX = FreePBX::Create();
+
+//run functions
+$Fme=$FreePBX->Findmefollow->get("1234,"0");
+
+//dump output
+echo print_r($Fme);
