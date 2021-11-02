f52127192251661792977c789a3a42605f04ebe8
Stephen Holinaty <sholinaty@vendasta.com>
2021-11-02 10:38:38 -0600

    Update testGetUser.php

--- php_scripts/testGetUser.php
+++ php_scripts/testGetUser.php
@@ -1,4 +1,4 @@
-// run this script as > sudo -u asterisk php /path/to/testGetUser.php
+// run this script as $sudo -u asterisk php /path/to/testGetUser.php
 
 //set up the environment
 <?php
@@ -6,7 +6,7 @@ include '/etc/freepbx.conf';
 $FreePBX = FreePBX::Create();
 
 //run functions
-$user=$FreePBX->Core->getUser("5133");
+$user=$FreePBX->Core->getUser("1234");
 
 //dump output
 echo print_r($user);
